444d238c00d5110032c5d5c5a8c5fd7b3162a2af
Mannan0007 <mauu0052@gmail.com>
2025-02-19 17:17:15 +0530

    main chat working

--- yap/server.js
+++ yap/server.js
@@ -4,8 +4,51 @@ import http from "http";
 import { Server } from "socket.io";
 connectDB()
 const server = http.createServer(app);
+const PORT = process.env.PORT || 2000;
 
-app.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on ${process.env.PORT}`);
+});
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000", // Update this based on your frontend URL
+  },
+});
+
+
+io.on("connection", (socket) => {
+  console.log("connected to the socket.io");
+  socket.on('setup', (userData) => {
+    socket.join(userData._id);
+    socket.emit('connected');
+  });
+
+  socket.on('join chat', (room) => {
+    socket.join(room);
+    console.log('User joined room ' + room);
+  })
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on('new message', (newMessageRecieved) => {
+        console.log("New Message Received:", newMessageRecieved);
+
+    var chat = newMessageRecieved.chat;
+
+     if (!chat.users) return console.log("chat.users not defined");
+
+      chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+        console.log("Message Sent To:", user._id);
+
+      socket.in(user._id).emit("message received", newMessageRecieved);
+    });
+  })
+  //   socket.off("setup", () => {
+  //   console.log("USER DISCONNECTED");
+  //   socket.leave(userData._id);
+  // });
 });
 

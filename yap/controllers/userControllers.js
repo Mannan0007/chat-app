e045040deeb97e7dcb92bbe55656878767201e2c
Mannan0007 <mauu0052@gmail.com>
2025-03-02 15:14:19 +0530

    final changes before deploying

--- yap/controllers/userControllers.js
+++ yap/controllers/userControllers.js
@@ -3,10 +3,11 @@ import User from '../models/userModel.js'
 import generateToken from "../config/generateToken.js";
 
 
-export const registerUser = expressAsyncHandler(async (req,res) => {
+export const registerUser = expressAsyncHandler(async (req, res) => {
     const { name, email, password, pic } = req.body;
 
     if (!name || !email || !password) {
+        res.status(400);
         throw new Error("Please enter all the fields");
     }
 
@@ -28,39 +29,36 @@ export const registerUser = expressAsyncHandler(async (req,res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            isAdmin: user.isAdmin,
             pic: user.pic,
-            token:generateToken(user._id)
+            token: generateToken(user._id)
         });
     }
     else {
         res.status(400);
-        throw new Error("Failed to create the user");
+        throw new Error("User not found");
     }
-
-    
-})
+});
 
 export const loginUser = expressAsyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const user=await User.findOne({email})
 
-    if (user &&(await user.matchPassword(password)) ) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            pic: user.pic,
-            token: generateToken(user._id),
-        })
-        
-    }
-    if (!user) {
-        res.status(401);
-        throw new Error("Invalid email or Password")
-    }
-    
-})
+if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
 
 
 export const allUsers = expressAsyncHandler(async (req, res) => {
